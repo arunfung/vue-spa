d8318d51e880d9455ca24f523aaf94678f95a647
冯治国 <fengzhiguo@julanling.com>
2018-01-23 15:23:46 +0800

    vue spa ok

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Vue SPA</title>
+        <link rel="stylesheet" href="/css/app.css">
+
+
+    </head>
+    <body>
+    <div id="app">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                {{--<div class="navbar-header">--}}
+                    {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">--}}
+                        {{--<span class="sr-only">Toggle navigation</span>--}}
+                        {{--<span class="icon-bar"></span>--}}
+                        {{--<span class="icon-bar"></span>--}}
+                        {{--<span class="icon-bar"></span>--}}
+                    {{--</button>--}}
+                    {{--<a class="navbar-brand" href="#">Brand</a>--}}
+                {{--</div>--}}
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><router-link to="/">Home</router-link></li>
+                        <router-link to="/about" tag="li">
+                            <a >about</a>
+                        </router-link>
+                    </ul>
+
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <router-view></router-view>
+    </div>
+
+    <script src="js/app.js"></script>
+    </body>
+</html>
