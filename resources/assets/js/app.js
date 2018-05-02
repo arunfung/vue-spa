d41389a1928660a25763917f3160098b9d254e7e
arun <arunfung@gmail.com>
2018-05-02 21:42:25 +0800

    修改配置

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,11 +12,16 @@ window.Vue = require('vue');
 import VueRouter from 'vue-router'
 import router from './routes'
 import App from './components/App'
-import VeeValidate from 'vee-validate';
+
+import zh_CN from 'vee-validate/dist/locale/zh_CN';
+import VeeValidate, { Validator } from 'vee-validate';
+Validator.localize('zh_CN', zh_CN);
 
 
 Vue.use(VueRouter);
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+    locale: 'zh_CN'
+});
 Vue.component('app', App);
 
 new Vue({
