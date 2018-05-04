4573373105eb86d805092580e3bf1d867a5a1cc1
arun <arunfung@gmail.com>
2018-05-04 22:57:07 +0800

    vuex管理登录状态

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,9 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+
 
 require('./bootstrap');
 
@@ -11,12 +11,25 @@ window.Vue = require('vue');
 
 import VueRouter from 'vue-router'
 import router from './routes'
+import store from './store/index'
+import jwtToken from './helpers/jwt'
 import App from './components/App'
 
 import zh_CN from 'vee-validate/dist/locale/zh_CN';
 import VeeValidate, { Validator } from 'vee-validate';
-Validator.localize('zh_CN', zh_CN);
 
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    if (jwtToken.getToken()) {
+        config.headers['Authorization'] = 'Bearer '+ jwtToken.getToken();
+    }
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+Validator.localize(zh_CN);
 
 Vue.use(VueRouter);
 Vue.use(VeeValidate, {
@@ -26,5 +39,6 @@ Vue.component('app', App);
 
 new Vue({
     el: '#app',
-    router
+    router,
+    store
 });
--- resources/assets/js/components/common/TopMenu.vue
+++ resources/assets/js/components/common/TopMenu.vue
@@ -7,12 +7,18 @@
             </div>
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <router-link to="/login" tag="li">
+                    <router-link v-if="!user.authenticated" to="/login" tag="li">
                         <a>login</a>
                     </router-link>
-                    <router-link to="/register" tag="li">
+                    <router-link v-if="!user.authenticated" to="/register" tag="li">
                         <a>register</a>
                     </router-link>
+                    <router-link v-if="user.authenticated" to="/profile" tag="li">
+                        <a>profile</a>
+                    </router-link>
+                    <li v-if="user.authenticated">
+                        <a>logout</a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -20,7 +26,12 @@
 </template>
 
 <script>
+    import {mapState} from 'vuex'
     export default {
-
+        computed: {
+            ...mapState({
+                user: state => state.AuthUser
+            })
+        }
     }
 </script>
